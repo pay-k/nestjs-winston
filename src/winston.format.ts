ed954665bec0bdca8397c5381f566b4840e3f3ab
danshapir <s.dan@live.com>
2019-09-19 16:47:19 +0300

    Fix export

--- src/winston.format.ts
+++ src/winston.format.ts
@@ -1,8 +1,6 @@
 import * as colors from 'colors/safe';
 import { format } from 'winston';
 
-const winstonConsoleFormat = format.printf(({ context, level, timestamp, message, ...meta }) => {
+export const winstonConsoleFormat = format.printf(({ context, level, timestamp, message, ...meta }) => {
     return `${level}: ${new Date(timestamp).toLocaleString()}\t [${colors.yellow(context)}] ${message} - ${JSON.stringify(meta)}`;
   });
-
-export default winstonConsoleFormat;
